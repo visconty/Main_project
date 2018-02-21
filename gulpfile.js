7f55b192d4b01a8b48edd51a78885d11915d4fbc
Vladimir Bespechny <vladimir.bespechny@p2h.com>
2018-02-21 10:28:32 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ const gulp = require('gulp');
 const concatCss = require('gulp-concat-css');
 const notify = require('gulp-notify');
 const autoprefixer = require('gulp-autoprefixer');
-const minifyCss = require('gulp-minify-css');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const uglifyJs = require('gulp-uglify');
 const concatJs = require('gulp-concat');
 const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const cmq = require('gulp-combine-mq');
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const cssnano = require('gulp-cssnano');
 
 const paths = {
   src: {
@@ -31,6 +35,9 @@ const paths = {
         './node_modules/swiper/dist/js/swiper.jquery.js',
         // './src/js/libs/detect.js'
       ]
+    },
+    images: {
+      all: 'src/images/*'
     }
   },
   web: {
@@ -53,6 +60,9 @@ const paths = {
         file: 'libs.js',
         dir: './web/js/libs'
       }
+    },
+    images: {
+      app: 'web/img'
     }
   }
 };
@@ -61,16 +71,24 @@ gulp.task('scss', function () {
   return gulp.src(paths.src.styles.app)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', notify.onError()))
-    .pipe(autoprefixer('last 5 versions', '> 1%', 'ie 9'))
-    .pipe(minifyCss())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cmq({
+      beautify: true
+    }))
+    .pipe(cssnano())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.web.styles.app.dir))
+    .pipe(gulp.dest(paths.web.styles.app.dir));
 });
 
 gulp.task('libsCss', function () {
   return gulp.src(paths.src.styles.libs)
     .pipe(concatCss(paths.web.styles.libs.file))
-    .pipe(minifyCss())
+    .pipe(cmq({
+      beautify: true
+    }))
     .pipe(gulp.dest(paths.web.styles.libs.dir));
 });
 
@@ -88,15 +106,25 @@ gulp.task('js', function () {
     .pipe(concatJs(paths.web.scripts.app.file))
     .pipe(uglifyJs().on('error', notify.onError()))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.web.scripts.app.dir))
-    .pipe(notify('Done!'));
+    .pipe(gulp.dest(paths.web.scripts.app.dir));
+});
+
+gulp.task('images', function() {
+  return gulp.src(paths.src.images.all)
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest(paths.web.images.app));
 });
 
 gulp.task('watch', function () {
-  gulp.watch([paths.src.styles.libs], ['libsCss']);
+  browserSync.init({
+    server: {
+      baseDir: './web'
+    }
+  });
   gulp.watch([paths.src.styles.appAll], ['scss']);
-  // gulp.watch([paths.src.scripts.libs], ['libsJs']  );
+  gulp.watch([paths.src.images.all], ['images']);
   gulp.watch([paths.src.scripts.appAll], ['js']);
+  browserSync.watch('**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('default', [
@@ -104,5 +132,6 @@ gulp.task('default', [
   'libsCss',
   'libsJs',
   'js',
+  'images',
   'watch'
 ]);
